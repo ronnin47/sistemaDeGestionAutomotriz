cd72e49f67c2670c91ee3b1bba9f7d2423c1171a
ronnin47 <tempesttempest66@gmail.com>
2026-06-29 16:09:49 -0300

    soluciones de nugets

--- Services/UsuarioService.cs
+++ Services/UsuarioService.cs
@@ -16,7 +16,7 @@ namespace sistemaDeGestionAutomotriz.Services
     {
 
 
-        //cambios
+        //aca estamos 
         public Usuario Login(string email, string pass)
         {
             using (NpgsqlConnection conexion = new NpgsqlConnection(Database.CadenaConexion))
